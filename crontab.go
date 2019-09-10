5a3bcac1df1ab94ed8513add3bdb8070f090af8c
abulo.hoo <abulo.hoo@gmail.com>
2019-09-11 00:04:54 +0800

    使用调度器

--- crontab.go
+++ crontab.go
@@ -1,308 +1,423 @@
+// sched := crontab.NewScheduler()
+// sched.Schedule().Every(10).Seconds().Do(something)
+// sched.Schedule().Every(3).Minutes().Do(something)
+// sched.Schedule().Every(4).Hours().Do(something)
+// sched.Schedule().Every(2).Days().At("12:32").Do(something)
+// sched.Schedule().Every(12).Weeks().Do(something)
+// sched.Schedule().Every(1).Monday().Do(something)
+// sched.Schedule().Every(1).Saturday().At("8:00").Do(something)
+// sched.Run()
 package crontab
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"reflect"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-// Crontab 表示cron表的Crontab结构
-type Crontab struct {
-	ticker *time.Ticker
-	jobs   []job
-}
+//TimeUnit 是用于在内部处理时间单位的计数
+type TimeUnit int
 
-// job 在cron表中工作
-type job struct {
-	second    map[int]struct{}
-	min       map[int]struct{}
-	hour      map[int]struct{}
-	day       map[int]struct{}
-	month     map[int]struct{}
-	dayOfWeek map[int]struct{}
-
-	fn   interface{}
-	args []interface{}
-}
-
-// tick 是每分钟发生的单个任务
-type tick struct {
-	second    int
-	min       int
-	hour      int
-	day       int
-	month     int
-	dayOfWeek int
-}
-
-// New 新的初始化并返回新的cron表
-func New() *Crontab {
-	return new(time.Minute)
-}
-
-// new 创建了新的crontab，arg用于测试目的
-func new(t time.Duration) *Crontab {
-	c := &Crontab{
-		ticker: time.NewTicker(t),
-	}
-
-	go func() {
-		for t := range c.ticker.C {
-			c.runScheduled(t)
-		}
-	}()
-
-	return c
-}
-
-// AddJob 到cron表
-func (c *Crontab) AddJob(schedule string, fn interface{}, args ...interface{}) error {
-	j, err := parseSchedule(schedule)
-	if err != nil {
-		return err
-	}
-
-	if fn == nil || reflect.ValueOf(fn).Kind() != reflect.Func {
-		return fmt.Errorf("Cron 必须是func（）")
-	}
-
-	fnType := reflect.TypeOf(fn)
-	if len(args) != fnType.NumIn() {
-		return fmt.Errorf("func（）参数的数量和提供的参数的数量不匹配")
-	}
-
-	for i := 0; i < fnType.NumIn(); i++ {
-		a := args[i]
-		t1 := fnType.In(i)
-		t2 := reflect.TypeOf(a)
-
-		if t1 != t2 {
-			if t1.Kind() != reflect.Interface {
-				return fmt.Errorf("Param with index %d shold be `%s` not `%s`", i, t1, t2)
-			}
-			if !t2.Implements(t1) {
-				return fmt.Errorf("Param with index %d of type `%s` doesn't implement interface `%s`", i, t2, t1)
-			}
-		}
-	}
-
-	// 全部选中，将作业添加到crontab
-	j.fn = fn
-	j.args = args
-	c.jobs = append(c.jobs, j)
-	return nil
-}
-
-// MustAddJob 就像AddJob，但如果作业有问题就会发生失败
-func (c *Crontab) MustAddJob(schedule string, fn interface{}, args ...interface{}) {
-	if err := c.AddJob(schedule, fn, args...); err != nil {
-		panic(err)
-	}
-}
-
-// Shutdown the cron table schedule
-func (c *Crontab) Shutdown() {
-	c.ticker.Stop()
-}
-
-// Clear all jobs from cron table
-func (c *Crontab) Clear() {
-	c.jobs = []job{}
-}
-
-// RunAll jobs in cron table, shcheduled or not
-func (c *Crontab) RunAll() {
-	for _, j := range c.jobs {
-		go j.run()
-	}
-}
-
-// RunScheduled jobs
-func (c *Crontab) runScheduled(t time.Time) {
-	tick := getTick(t)
-	for _, j := range c.jobs {
-		if j.tick(tick) {
-			go j.run()
-		}
-	}
-}
-
-// run the job using reflection
-func (j job) run() {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("Crontab error", r)
-		}
-	}()
-	v := reflect.ValueOf(j.fn)
-	rargs := make([]reflect.Value, len(j.args))
-	for i, a := range j.args {
-		rargs[i] = reflect.ValueOf(a)
-	}
-	v.Call(rargs)
-}
-
-// tick decides should the job be lauhcned at the tick
-func (j job) tick(t tick) bool {
-
-	if _, ok := j.second[t.second]; !ok {
-		return false
-	}
-
-	if _, ok := j.min[t.min]; !ok {
-		return false
-	}
-
-	if _, ok := j.hour[t.hour]; !ok {
-		return false
-	}
-
-	_, day := j.day[t.day]
-	_, dayOfWeek := j.dayOfWeek[t.dayOfWeek]
-	if !day && !dayOfWeek {
-		return false
-	}
-
-	if _, ok := j.month[t.month]; !ok {
-		return false
-	}
-
-	return true
-}
-
-// 用于解析调度字符串的正则表达式
-var (
-	matchSpaces = regexp.MustCompile("\\s+")
-	matchN      = regexp.MustCompile("(.*)/(\\d+)")
-	matchRange  = regexp.MustCompile("^(\\d+)-(\\d+)$")
+const (
+	none = iota
+	second
+	minute
+	hour
+	day
+	week
+	monday
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+	sunday
 )
 
-// parseSchedule 创建具有填充时间的作业结构以启动，或者如果synthax错误则创建错误
-func parseSchedule(s string) (j job, err error) {
-	s = matchSpaces.ReplaceAllLiteralString(s, " ")
-	parts := strings.Split(s, " ")
-	if len(parts) != 6 {
-		return job{}, errors.New("Schedule string must have five components like * * * * *")
-	}
+var timeNow = func() time.Time {
+	return time.Now()
+}
 
-	j.second, err = parsePart(parts[0], 0, 59)
-	if err != nil {
-		return j, err
-	}
+// Job 结构处理调度和运行jobs所需的所有数据
+type Job struct {
+	identifier string
+	scheduler  *Scheduler
+	unit       TimeUnit
+	frequency  int
+	useAt      bool
+	atHour     int
+	atMinute   int
+	workFunc   func()
 
-	j.min, err = parsePart(parts[1], 0, 59)
-	if err != nil {
-		return j, err
-	}
+	nextScheduledRun time.Time
+}
 
-	j.hour, err = parsePart(parts[2], 0, 23)
-	if err != nil {
-		return j, err
-	}
+// Every 是以既定频率填充既定Job结构的方法
+func (j *Job) Every(frequencies ...int) *Job {
+	l := len(frequencies)
 
-	j.day, err = parsePart(parts[3], 1, 31)
-	if err != nil {
-		return j, err
-	}
-
-	j.month, err = parsePart(parts[4], 1, 12)
-	if err != nil {
-		return j, err
-	}
-
-	j.dayOfWeek, err = parsePart(parts[5], 0, 6)
-	if err != nil {
-		return j, err
-	}
-
-	switch {
-	case len(j.day) < 31 && len(j.dayOfWeek) == 7:
-		j.dayOfWeek = make(map[int]struct{})
-	case len(j.dayOfWeek) < 7 && len(j.day) == 31:
-		j.day = make(map[int]struct{})
+	switch l {
+	case 0:
+		j.frequency = 1
+	case 1:
+		if frequencies[0] <= 0 {
+			panic("Every expects frequency to be greater than of equal to 1")
+		}
+		j.frequency = frequencies[0]
 	default:
-
+		panic("Every expects 0 or 1 arguments")
 	}
 
-	return j, nil
+	return j
 }
 
-// parsePart 从日程表字符串中解析单个日程表部分
-func parsePart(s string, min, max int) (map[int]struct{}, error) {
+// At 方法使用提供的信息填充atHour和atMinute字段的Job结构
+func (j *Job) At(t string) *Job {
+	j.useAt = true
+	j.atHour, _ = strconv.Atoi(strings.Split(t, ":")[0])
+	j.atMinute, _ = strconv.Atoi(strings.Split(t, ":")[1])
+	return j
+}
 
-	r := make(map[int]struct{}, 0)
+// Do 执行方法
+func (j *Job) Do(function func()) string {
+	j.workFunc = function
+	j.scheduleNextRun()
+	j.scheduler.jobs = append(j.scheduler.jobs, *j)
+	return j.identifier
+}
 
-	if s == "*" {
-		for i := min; i <= max; i++ {
-			r[i] = struct{}{}
+func (j *Job) due() bool {
+	return timeNow().After(j.nextScheduledRun)
+}
+
+func (j *Job) isAtUsedIncorrectly() bool {
+	return j.useAt == true && (j.unit == second || j.unit == minute ||
+		j.unit == hour || j.unit == week)
+}
+
+func (j *Job) unitNotDayOrWEEKDAY() bool {
+	return j.unit == second || j.unit == minute ||
+		j.unit == hour || j.unit == week
+
+}
+
+func (j *Job) unitNotWEEKDAY() bool {
+	return j.unit == second || j.unit == minute ||
+		j.unit == hour || j.unit == day ||
+		j.unit == week
+}
+
+func (j *Job) scheduleNextRun() {
+	if j.frequency == 1 {
+		if j.isAtUsedIncorrectly() {
+			panic(
+				`Cannot schedule Every(1) with At()
+				 when unit is not day or WEEKDAY`,
+			) // TODO: Turn this into err
 		}
-		return r, nil
-	}
 
-	// */2 1-59/5 pattern
-	if matches := matchN.FindStringSubmatch(s); matches != nil {
-		localMin := min
-		localMax := max
-		if matches[1] != "" && matches[1] != "*" {
-			if rng := matchRange.FindStringSubmatch(matches[1]); rng != nil {
-				localMin, _ = strconv.Atoi(rng[1])
-				localMax, _ = strconv.Atoi(rng[2])
-				if localMin < min || localMax > max {
-					return nil, fmt.Errorf("Out of range for %s in %s. %s must be in range %d-%d", rng[1], s, rng[1], min, max)
+		if j.unitNotDayOrWEEKDAY() {
+			if j.nextScheduledRun == (time.Time{}) {
+				j.nextScheduledRun = timeNow()
+			}
+
+			switch j.unit {
+			case second:
+				j.nextScheduledRun = j.nextScheduledRun.Add(1 * time.Second)
+			case minute:
+				j.nextScheduledRun = j.nextScheduledRun.Add(1 * time.Minute)
+			case hour:
+				j.nextScheduledRun = j.nextScheduledRun.Add(1 * time.Hour)
+			case week:
+				// 168 hours in a week
+				j.nextScheduledRun = j.nextScheduledRun.Add(168 * time.Hour)
+			}
+		} else {
+			switch j.unit {
+			case day:
+				if j.nextScheduledRun == (time.Time{}) {
+					now := timeNow()
+					lastMidnight := time.Date(
+						now.Year(),
+						now.Month(),
+						now.Day(),
+						0, 0, 0, 0,
+						time.Local,
+					)
+					if j.useAt == true {
+						j.nextScheduledRun = lastMidnight.Add(
+							time.Duration(j.atHour)*time.Hour +
+								time.Duration(j.atMinute)*time.Minute,
+						)
+					} else {
+						// If At is not specified, move the next scheduled run to next midnight
+						j.nextScheduledRun = lastMidnight.Add(24 * time.Hour)
+					}
+				} else {
+					j.nextScheduledRun = j.nextScheduledRun.Add(24 * time.Hour)
+				}
+
+			case monday:
+				j.scheduleWeekday(time.Monday)
+			case tuesday:
+				j.scheduleWeekday(time.Tuesday)
+			case wednesday:
+				j.scheduleWeekday(time.Wednesday)
+			case thursday:
+				j.scheduleWeekday(time.Thursday)
+			case friday:
+				j.scheduleWeekday(time.Friday)
+			case saturday:
+				j.scheduleWeekday(time.Saturday)
+			case sunday:
+				j.scheduleWeekday(time.Sunday)
+			}
+
+		}
+
+		fmt.Println("Scheduled for ", j.nextScheduledRun)
+
+	} else {
+		if j.isAtUsedIncorrectly() {
+			panic("Cannot schedule Every(>1) with At() when unit is not day")
+		}
+
+		if j.unitNotWEEKDAY() {
+
+			if j.unit != day {
+				if j.nextScheduledRun == (time.Time{}) {
+					j.nextScheduledRun = timeNow()
+				}
+
+				switch j.unit {
+				case second:
+					j.nextScheduledRun = j.nextScheduledRun.Add(
+						time.Duration(j.frequency) * time.Second,
+					)
+				case minute:
+					j.nextScheduledRun = j.nextScheduledRun.Add(
+						time.Duration(j.frequency) * time.Minute,
+					)
+				case hour:
+					j.nextScheduledRun = j.nextScheduledRun.Add(
+						time.Duration(j.frequency) * time.Hour,
+					)
+				case week:
+					j.nextScheduledRun = j.nextScheduledRun.Add(
+						time.Duration(j.frequency*168) * time.Hour,
+					)
+
 				}
 			} else {
-				return nil, fmt.Errorf("Unable to parse %s part in %s", matches[1], s)
-			}
-		}
-		n, _ := strconv.Atoi(matches[2])
-		for i := localMin; i <= localMax; i += n {
-			r[i] = struct{}{}
-		}
-		return r, nil
-	}
+				if j.nextScheduledRun == (time.Time{}) {
+					now := timeNow()
+					lastMidnight := time.Date(
+						now.Year(),
+						now.Month(),
+						now.Day(),
+						0, 0, 0, 0,
+						time.Local,
+					)
+					if j.useAt == true {
+						j.nextScheduledRun = lastMidnight.Add(
+							time.Duration(j.atHour)*time.Hour +
+								time.Duration(j.atMinute)*time.Minute,
+						)
+					} else {
+						j.nextScheduledRun = lastMidnight
+					}
+				}
 
-	// 1,2,4  or 1,2,10-15,20,30-45 pattern
-	parts := strings.Split(s, ",")
-	for _, x := range parts {
-		if rng := matchRange.FindStringSubmatch(x); rng != nil {
-			localMin, _ := strconv.Atoi(rng[1])
-			localMax, _ := strconv.Atoi(rng[2])
-			if localMin < min || localMax > max {
-				return nil, fmt.Errorf("Out of range for %s in %s. %s must be in range %d-%d", x, s, x, min, max)
+				j.nextScheduledRun = j.nextScheduledRun.Add(
+					time.Duration(j.frequency*24) * time.Hour,
+				)
+
 			}
-			for i := localMin; i <= localMax; i++ {
-				r[i] = struct{}{}
-			}
-		} else if i, err := strconv.Atoi(x); err == nil {
-			if i < min || i > max {
-				return nil, fmt.Errorf("Out of range for %d in %s. %d must be in range %d-%d", i, s, i, min, max)
-			}
-			r[i] = struct{}{}
+
 		} else {
-			return nil, fmt.Errorf("Unable to parse %s part in %s", x, s)
+			panic("Cannot schedule Every(>1) when unit is WEEKDAY")
 		}
-	}
 
-	if len(r) == 0 {
-		return nil, fmt.Errorf("Unable to parse %s", s)
-	}
+		fmt.Println("Scheduled for ", j.nextScheduledRun)
 
-	return r, nil
+	}
+	return
 }
 
-// getTick 从时间返回tick结构
-func getTick(t time.Time) tick {
-	return tick{
-		second:    t.Second(),
-		min:       t.Minute(),
-		hour:      t.Hour(),
-		day:       t.Day(),
-		month:     int(t.Month()),
-		dayOfWeek: int(t.Weekday()),
+func (j *Job) scheduleWeekday(dayOfWeek time.Weekday) {
+	if j.nextScheduledRun == (time.Time{}) {
+		now := timeNow()
+		lastWeekdayMidnight := time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day()-int(now.Weekday()-dayOfWeek),
+			0, 0, 0, 0,
+			time.Local)
+		if j.useAt == true {
+			j.nextScheduledRun = lastWeekdayMidnight.Add(
+				time.Duration(j.atHour)*time.Hour +
+					time.Duration(j.atMinute)*time.Minute,
+			)
+		} else {
+			j.nextScheduledRun = lastWeekdayMidnight
+		}
 	}
+	j.nextScheduledRun = j.nextScheduledRun.Add(7 * 24 * time.Hour)
+}
+
+// Second 方法用秒填充给定的作业结构
+func (j *Job) Second() *Job {
+	j.unit = second
+	return j
+}
+
+// Seconds 方法用秒填充给定的作业结构
+func (j *Job) Seconds() *Job {
+	j.unit = second
+	return j
+}
+
+// Minute 方法用分钟填充给定的作业结构
+func (j *Job) Minute() *Job {
+	j.unit = minute
+	return j
+}
+
+// Minutes 方法用分钟填充给定的作业结构
+func (j *Job) Minutes() *Job {
+	j.unit = minute
+	return j
+}
+
+// Hour 方法用小时填充给定的作业结构
+func (j *Job) Hour() *Job {
+	j.unit = hour
+	return j
+}
+
+// Hours 方法用小时填充给定的作业结构
+func (j *Job) Hours() *Job {
+	j.unit = hour
+	return j
+}
+
+// Day 方法用每天填充给定的作业结构
+func (j *Job) Day() *Job {
+	j.unit = day
+	return j
+}
+
+// Days 方法用每天填充给定的作业结构
+func (j *Job) Days() *Job {
+	j.unit = day
+	return j
+}
+
+// Week 方法用每周填充给定的作业结构
+func (j *Job) Week() *Job {
+	j.unit = week
+	return j
+}
+
+// Weeks 方法用每周填充给定的作业结构
+func (j *Job) Weeks() *Job {
+	j.unit = week
+	return j
+}
+
+// Monday 周一
+func (j *Job) Monday() *Job {
+	j.unit = monday
+	return j
+}
+
+// Tuesday 周二
+func (j *Job) Tuesday() *Job {
+	j.unit = tuesday
+	return j
+}
+
+// Wednesday 周3
+func (j *Job) Wednesday() *Job {
+	j.unit = wednesday
+	return j
+}
+
+// Thursday 周4
+func (j *Job) Thursday() *Job {
+	j.unit = thursday
+	return j
+}
+
+// Friday 周5
+func (j *Job) Friday() *Job {
+	j.unit = friday
+	return j
+}
+
+// Saturday 周6
+func (j *Job) Saturday() *Job {
+	j.unit = saturday
+	return j
+}
+
+// Sunday 周7
+func (j *Job) Sunday() *Job {
+	j.unit = sunday
+	return j
+}
+
+// Scheduler 存储 job
+type Scheduler struct {
+	identifier string
+	jobs       []Job
+}
+
+// NewScheduler 创建一个调度器 new Scheduler
+func NewScheduler() Scheduler {
+	return Scheduler{
+		identifier: uuid.New().String(),
+		jobs:       make([]Job, 0),
+	}
+}
+
+func (s *Scheduler) activateTestMode() {
+	timeNow = func() time.Time {
+		return time.Date(1, 1, 1, 1, 1, 0, 0, time.Local)
+	}
+}
+
+// Run method  Scheduler.
+func (s *Scheduler) Run() {
+	for {
+		for jobIdx := range s.jobs {
+			job := &s.jobs[jobIdx]
+			if job.due() {
+				job.scheduleNextRun()
+				go job.workFunc()
+			}
+		}
+		time.Sleep(1 * time.Second)
+
+	}
+}
+
+// Schedule
+func (s *Scheduler) Schedule() *Job {
+	newJob := Job{
+		identifier:       uuid.New().String(),
+		scheduler:        s,
+		unit:             none,
+		frequency:        1,
+		useAt:            false,
+		atHour:           0,
+		atMinute:         0,
+		workFunc:         nil,
+		nextScheduledRun: time.Time{}, // zero value
+	}
+	return &newJob
 }
